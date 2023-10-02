ec8f3923b6cfa5befc21918c74867ff78554d610
David Ruiz <druizg@gmail.com>
2023-10-02 18:20:43 +0200

    Icons for Web

---
+++ SampleIcons.Web/SampleIcons.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SampleIcons.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
