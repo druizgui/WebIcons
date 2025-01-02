d32395c3501847e1b5b2c2b30bb7b701a766802a
David Ruiz <druizg@gmail.com>
2025-01-02 18:23:57 +0100

    web icons

---
+++ SampleIcons.Web/SampleIcons.Web/Controllers/ErrorController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace SampleIcons.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("error/404")]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult PageNotFound()
+        {
+            Response.Clear();
+            Response.StatusCode = StatusCodes.Status404NotFound;
+            return View("PageNotFound");
+        }
+
+        [Route("error/{code}")]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Index(int code)
+        {
+            Response.Clear();
+            Response.StatusCode = code;
+            return View("GenericError", Response.StatusCode);
+        }
+
+        [Route("error/handle-exception")]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult HandleException()
+        {
+            // log error
+            var feature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            if (feature != null)
+            {
+                //_logger.LogException(feature.Error);
+            }
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+
+        [Route("error/unhandled-exception")]
+        public IActionResult TestNotImplementedException() => throw new NotImplementedException();
+    }
+}
--- SampleIcons.Web/SampleIcons.Web/Program.cs
+++ SampleIcons.Web/SampleIcons.Web/Program.cs
@@ -8,10 +8,16 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+
+    //app.UseExceptionHandler("/Home/Error"); 
+    //app.UseHsts();
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
+
+app.UseExceptionHandler("/error/handle-exception");
+app.UseStatusCodePagesWithReExecute("/error/{0}");
+app.UseHsts();
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
